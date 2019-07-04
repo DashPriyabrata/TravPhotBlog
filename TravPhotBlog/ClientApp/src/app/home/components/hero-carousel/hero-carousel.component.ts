d6c86a6196ded8ee12bb89a42d89a92cefc4c754
Priyabrata Dash <priyabrata.dash@adelphi.digital>
2019-07-04 19:53:20 +0800

    Hero carousel final fix to show image from db

--- TravPhotBlog/ClientApp/src/app/home/components/hero-carousel/hero-carousel.component.ts
+++ TravPhotBlog/ClientApp/src/app/home/components/hero-carousel/hero-carousel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as $ from 'jquery';
 import { FeaturedPostsService } from '../../../core/services/featured-posts.service';
 
 import { BlogPost } from '../../../core/models/blog-post';
@@ -8,17 +9,20 @@ import { BlogPost } from '../../../core/models/blog-post';
   templateUrl: './hero-carousel.component.html',
   styleUrls: ['./hero-carousel.component.css']
 })
-export class HeroCarouselComponent implements OnInit {
+export class HeroCarouselComponent implements OnInit, AfterViewInit {
   featuredPosts: BlogPost[];
-
 
   constructor(private featuredPostsService: FeaturedPostsService) { }
 
   ngOnInit() {
     this.featuredPostsService.getFeaturedPosts().subscribe((res) => {
       this.featuredPosts = res;
-      //debugger;
     });
   }
 
+  ngAfterViewInit() {
+    $(".sayit_slider_item_wrapper").each(function () {
+      $(this).css('background-image', 'url(' + $(this).attr('data-src') + ')');
+    });
+  }
 }
