76d415db575777f4ec4170af07aff9d265466666
Priyabrata Dash <priyabrata.dash@adelphi.digital>
2019-07-11 18:18:50 +0800

    Post info service works in post component

--- TravPhotBlog/ClientApp/src/app/core/services/blog-info.service.ts
+++ TravPhotBlog/ClientApp/src/app/core/services/blog-info.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { HttpParams } from "@angular/common/http";
 import { Constants } from '../../configs/constants';
 
 import { BlogPost } from '../models/blog-post';
@@ -16,7 +15,6 @@ export class BlogInfoService {
   constructor(private httpClient: HttpClient) { }
 
   public getBlogInfo(blogId: string) {
-    const params = new HttpParams().set('blogId', blogId);
-    return this.httpClient.get<BlogPost>(this.blogInfoApiURL, { params });
+    return this.httpClient.get<BlogPost>(this.blogInfoApiURL + "/" + blogId);
   }
 }
--- TravPhotBlog/ClientApp/src/app/post/post.component.ts
+++ TravPhotBlog/ClientApp/src/app/post/post.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { BlogInfoService } from '../core/services/blog-info.service';
-
+import * as $ from 'jquery';
 import { BlogPost } from '../core/models/blog-post';
 
 @Component({
@@ -9,23 +9,25 @@ import { BlogPost } from '../core/models/blog-post';
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent implements OnInit {
+export class PostComponent implements OnInit, AfterViewInit {
 
   private blogId: string;
   blogData: BlogPost;
 
-  constructor(private route: ActivatedRoute, private blogInfoService: BlogInfoService) { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private blogInfoService: BlogInfoService) {
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.blogId = params.get('blogId');
-      debugger
-    });
-
-    this.blogInfoService.getBlogInfo(this.blogId).subscribe((res) => {
-      debugger;
-      this.blogData = res;
     });
   }
 
+  ngOnInit() {
+    this.blogInfoService.getBlogInfo(this.blogId).subscribe((res) => {
+      this.blogData = res;
+    });
+  }
+  ngAfterViewInit() {
+    $(".sayit_title_container").each(function () {
+      $(this).css('background-image', 'url(' + $(this).attr('data-src') + ')');
+    });
+  }
 }
