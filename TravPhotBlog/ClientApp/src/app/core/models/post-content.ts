cd056b3b076d1bc67eab36a5a279dedbdd054522
Priyabrata Dash <priyabrata.dash@adelphi.digital>
2019-07-23 18:28:19 +0800

    Image src & alt binding added to post content page

--- TravPhotBlog/ClientApp/src/app/core/models/post-content.ts
+++ TravPhotBlog/ClientApp/src/app/core/models/post-content.ts
@@ -5,12 +5,16 @@ export class PostContent {
   public Quote: Date;
   public Description_Start: string;
   public Image_BeforeHighlights: string;
+  public ImageStartAlt: string;
   public Highlights: boolean;
   public Image_AfterHighlights: number;
+  public ImageUpperMidAlt: string;
   public Description_End: string;
   public Image_End: boolean;
+  public ImageLowerMidAlt: string;
   public Closing_Statements: boolean;
   public Image_Direction: number;
+  public ImageDirectionAlt: string;
   public Direction: number;
   public ArrivingHere: number;
   public Attractions: string;
