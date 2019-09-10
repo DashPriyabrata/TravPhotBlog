67f6fa1ef9f2c591173652b1c4a9307766b4c203
Priyabrata Dash <priyabrata.dash@adelphi.digital>
2019-09-10 16:16:01 +0800

    Ngx Page Scroll Angular package implementaion for comment reply

--- TravPhotBlog/ClientApp/src/app/post/post.module.ts
+++ TravPhotBlog/ClientApp/src/app/post/post.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { NgxPageScrollModule } from 'ngx-page-scroll';
 //import { PostRoutingModule } from './post-routing.module';
 import { PostComponent } from './post.component';
 import { BlogCommentComponent } from '../shared/components/blog-comment/blog-comment.component';
@@ -12,7 +12,8 @@ import { ParentIdPipe } from '../shared/pipes/parent-id.pipe';
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxPageScrollModule
     //PostRoutingModule
   ]
 })
