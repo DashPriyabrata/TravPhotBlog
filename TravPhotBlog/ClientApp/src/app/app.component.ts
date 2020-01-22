b0c3f176c7f6cf80ac752b813c0a9c2837a423e7
Priyabrata Dash <priyabrata.dash@adelphi.digital>
2020-01-22 15:22:49 +0800

    [#40]Canonical url SEO implementation

--- TravPhotBlog/ClientApp/src/app/app.component.ts
+++ TravPhotBlog/ClientApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CanonicalService } from './core/services/canonical.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ClientApp';
+
+  constructor(private canonicalService: CanonicalService) { }
+
+  ngOnInit() {
+    this.canonicalService.setCanonicalURL();
+  }
 }
